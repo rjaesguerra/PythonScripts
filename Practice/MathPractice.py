b9b74e415995ef7a3db7b67db573acd36368c189
rjaesguerra <rjaesguerra@gmail.com>
2017-09-07 22:19:25 -0700

    Optimizing Prime Number checker and adding a main run method

--- Practice/MathPractice.py
+++ Practice/MathPractice.py
@@ -2,25 +2,34 @@
 
 def check_if_prime_number(num):
     """Print if a number is a prime number or not and display its factors"""
+    status = False
     if (num < 2):
         print(num, "is not a prime number.")
 
     # To make computation more efficient, you can split the range by half
-    for x in range(2,num):
-        if (num%x == 0):
-            print(num, "is not a prime number.")
-            print(num, "has a factor of", x, "and", int(num/x) )
-            break
     else:
-        print(num, "is a prime number.")
+        for x in range(2,num):
+            if (num%x == 0):
+                print(num, "is not a prime number.")
+                print(num, "has a factor of", x, "and", int(num/x), "." )
+                break
+        else:
+            print(num, "is a prime number.")
+            status = True
 
-    return
+    return status
 
 def display_fibonacci(lastNum):
     """Display the Fibonacci sequence up until the input number"""
     a = 0
     b = 1
+    print("Fibonacci Sequence: ")
     while(b < lastNum):
-        print(b)
+        print(b, end=" ")
         a, b = b, a+b
     return
+
+if __name__ == "__main__":
+    check_if_prime_number(29)
+    check_if_prime_number(30)
+    display_fibonacci(2000)
